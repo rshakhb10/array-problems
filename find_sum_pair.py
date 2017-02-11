db51e2a6c5654e9195f23589fbc9368cc9abd736
rshakhb10 <rsfbb05@gmail.com>
2017-02-11 18:26:22 -0500

    Create function find_sum_pair.

---
+++ find_sum_pair.py
@@ -0,0 +1,29 @@
+'''Given array find a pair of elements with sum equal to given number.'''
+
+def find_sum_pair(numbers, sum):
+   
+    numbers_dict = {}
+    for n in numbers:
+        if not numbers_dict.has_key(n):
+            numbers_dict[n] = 1
+        else:
+            numbers_dict[n]+=1
+
+
+    for n_key in numbers_dict:
+        # find difference between current number and the sum
+        # check if difference is in the dictionary
+        diff = sum - n_key
+        if numbers_dict.has_key(diff):
+            print "Found numbers ", n_key, " and ", diff, " which sum equals to ", sum
+            #numbers_dict.pop(n_key)
+            #numbers_dict.pop(diff)
+
+
+
+# Main #
+numbers = [6, 12, 2, 7, 11, 3]
+find_sum_pair(numbers, 9)
+
+
+    
